47e72bc85fc384564c29dc98ef13389c2bffcca9
IvanFadieiev <ivanfadeev91@gmail.com>
2016-07-06 09:14:50 +0000

    config for shopify

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "e94c616fc3c4c3ac8611dbe2e8c9608b"
-  config.secret = "83e831acf036f45437ddbe2c4c244851"
-  config.redirect_uri = "http://test-app-developerweb.c9users.io/auth/shopify/callback"
+  config.secret = "f12c7ca57f44556f418cf1b89ea5aadd"
+  config.redirect_uri = "https://test-app-developerweb.c9users.io/auth/shopify/callback"
   config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
 end
